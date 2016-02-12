7abefd98949d0671e348136c93c5923b0b99c186
Me <ratsbestfriend@yahoo.com>
2016-02-11 21:44:36 -0800

    Initial Commit

---
+++ tapperApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tapperApp
+//
+//  Created by strangerInDisguise on 2/11/16.
+//  Copyright © 2016 criticalEntertainment. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
