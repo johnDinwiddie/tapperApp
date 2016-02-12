0a9f757e2e4a2a29c45c99a779c079328dc9779c
strangerInDisguise <strangerInDisguise@su.attlocal.net>
2016-02-12 12:54:40 -0800

    This is my completed progect.

--- tapperApp/ViewController.swift
+++ tapperApp/ViewController.swift
@@ -9,17 +9,76 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //properties
+    var maxTaps = 0
+    var currentTaps = 0
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var backGroundImage: UIImageView!
+    @IBOutlet weak var coolText: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    
+    //hidden before button pressed
+    @IBOutlet weak var coconutButton: UIButton!
+    @IBOutlet weak var palmTreeImg: UIImageView!
+    @IBOutlet weak var tapsLabel: UILabel!
+    
+    //Actions
+    @IBAction func onCoconutButtonTapped(sender: UIButton!){
+        currentTaps++
+        updateTapsLabel()
+        
+        if isGameOver(){
+            restartGame()
+        }
     }
+    
+    @IBAction func onPlayButtonPressed(sender: UIButton) {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            
+            coolText.hidden = true
+            howManyTapsTxt.hidden = true
+            playButton.hidden = true
+            
+            coconutButton.hidden=false
+            palmTreeImg.hidden=false
+            tapsLabel.hidden = false
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            updateTapsLabel()
+        }
+        
     }
-
-
+    
+    func restartGame() {
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        
+        coolText.hidden = false
+        howManyTapsTxt.hidden = false
+        playButton.hidden = false
+        
+        coconutButton.hidden = true
+        palmTreeImg.hidden = true
+        tapsLabel.hidden = true
+    }
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps{
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func updateTapsLabel() {
+        tapsLabel.text = "\(currentTaps) Taps"
+        
+    }
+    
 }
 
